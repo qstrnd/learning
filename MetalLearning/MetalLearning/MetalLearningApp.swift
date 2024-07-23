a2a270b32a4ff7354945ce71a76d9a7817f663ff
Andy <a.iakovlev@proton.me>
2024-07-23 22:10:49 +0400

    Init MetalLearning project

---
+++ MetalLearning/MetalLearning/MetalLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLearningApp.swift
+//  MetalLearning
+//
+//  Created by Andy on 2024-07-23.
+//
+
+import SwiftUI
+
+@main
+struct MetalLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
