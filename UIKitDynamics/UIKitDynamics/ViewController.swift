a6cde566b9ef2587d4e9cadca139d6428335f1c2
Andy <a.iakovlev@proton.me>
2024-07-23 21:43:20 +0400

    Add Learning workspace

---
+++ UIKitDynamics/UIKitDynamics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitDynamics
+//
+//  Created by Andy on 2024-07-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
