ad3816fb16cdc236764f4585027ccfc9d3962d71
Andy <a.iakovlev@proton.me>
2025-01-08 20:51:11 +0100

    Add solution for problem 67

---
+++ LeetCode/String.playground/Pages/67_add_binary.xcplaygroundpage/Contents.swift
@@ -0,0 +1,63 @@
+/*:
+ [67. Add binary](https://leetcode.com/problems/add-binary/description/)
+ 
+ #### Solution
+ 
+ Simulate binary addition as how a human would do it.
+ 
+ - **Time Complexity**: _O(n + m)_, where n is the length of a and m is the length of m
+ - **Space Complexity**: _O(n + m)_, where n is the length of a and m is the length of m
+
+ */
+
+import Foundation
+
+final class Solution {
+    
+    func addBinary(_ a: String, _ b: String) -> String {
+        if a.isEmpty { return b }
+        if b.isEmpty { return a }
+        
+        let a = Array(a)
+        let b = Array(b)
+        var i = a.count - 1
+        var j = b.count - 1
+        var carry = 0
+        var result: [Int] = []
+        
+        while i >= 0 || j >= 0 || carry > 0 {
+            var sum = carry
+            
+            if i >= 0, a[i] == "1" { sum += 1 }
+            if j >= 0, b[j] == "1" { sum += 1 }
+            
+            carry = sum >= 2 ? 1 : 0
+            let digit = sum % 2
+            result.append(digit)
+            
+            i -= 1
+            j -= 1
+        }
+        
+        return result.reversed().reduce(into: "") { partialResult, val in
+            partialResult.append(String(val))
+        }
+    }
+    
+}
+
+// MARK: - Tests
+
+func testSolution() {
+    let solution = Solution()
+
+    assert(solution.addBinary("11", "1") == "100", "Test failed: addBinary(\"11\", \"1\") == \"100\"")
+    print("Test passed: addBinary(\"11\", \"1\") == \"100\"")
+    
+    assert(solution.addBinary("1010", "") == "1010", "Test failed: addBinary(\"1010\", \"\") == \"1010\"")
+    print("Test passed: addBinary(\"1010\", \"\") == \"1010\"")
+}
+
+testSolution()
+
+//: [Previous](@previous) || [Next](@next)
